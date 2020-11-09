023a0f55682e11e7228b2c73b94a4ecc03c33ddb
niyunsheng <ni-ys13@qq.com>
2020-11-09 21:51:43 +0800

    oj daily

--- LeetCodeC++/973. 最接近原点的 K 个点.cpp
+++ LeetCodeC++/973. 最接近原点的 K 个点.cpp
@@ -15,3 +15,29 @@ public:
         return ans;
     }
 };
+
+// 方法2：结构体加friend函数
+struct node{
+    int rank,norm;
+    node(int rank, int norm):rank(rank),norm(norm){}
+    friend bool operator<(node a, node b){
+        return a.norm > b.norm;
+    }
+};
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
+        priority_queue<node> pq;
+        for(int i=0;i<points.size();i++){
+            node t = node(i,points[i][0]*points[i][0]+points[i][1]*points[i][1]);
+            pq.push(t);
+        }
+        vector<vector<int>> ans;
+        for(int i=0;i<K&&!pq.empty();i++){
+            node t = pq.top();
+            ans.push_back(points[t.rank]);
+            pq.pop();
+        }
+        return ans;
+    }
+};
